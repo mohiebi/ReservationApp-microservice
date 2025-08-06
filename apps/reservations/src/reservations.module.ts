312cc9c9fb6b89ce8442cd9ac7072a529f4dc471
Mohi <e.mohamadhosein@gmail.com>
2025-08-06 14:42:21 +0330

    moving everything from reservation dir to src

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
-import { DatabaseModule } from '@app/common';
-import { ReservationsModule } from './reservations/reservations.module';
+import { ReservationsController } from './reservations.controller';
 
 @Module({
-  imports: [DatabaseModule, ReservationsModule],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
-export class ReservationsModule { }
+export class ReservationsModule {}
