39c899a6c4bb87ff795e51f53c200caccfb215d7
Mohi <e.mohamadhosein@gmail.com>
2025-08-08 17:23:00 +0330

    finishing touches as reservation service implementation

---
+++ apps/reservations/src/reservations.repository.ts
@@ -0,0 +1,14 @@
+import { AbstractRepositoy } from "@app/common";
+import { Injectable, Logger } from "@nestjs/common";
+import { ReservationDocument } from "./models/reservation.schema";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+
+@Injectable()
+export class ReservationsRepository extends AbstractRepositoy<ReservationDocument> {
+    protected readonly logger = new Logger(ReservationsRepository.name)
+
+    constructor(@InjectModel(ReservationDocument.name) reservationModel: Model<ReservationDocument>) {
+        super(reservationModel);
+    }
+}
