209cbde3574c2eb86208cfe59da023123a3d4aa6
Mohi <e.mohamadhosein@gmail.com>
2025-08-10 10:26:37 +0330

    adding port to .env

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
     whitelist: true,
   }));
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
