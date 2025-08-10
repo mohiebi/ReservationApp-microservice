54b1ba994638d6cd2360e68b3031ffa3966c2ae1
Mohi <e.mohamadhosein@gmail.com>
2025-08-10 12:35:27 +0330

    finishing jwt startegy

---
+++ apps/auth/src/Strategies/jwtstrategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { UsersService } from "./../users/users.service";
+import { ConfigService } from "@nestjs/config";
+import { Request } from 'express';
+import { tokenPayload } from "../interfaces/token-payload.interfaces";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly userService: UsersService,
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                (request: Request) => request?.cookies?.Authentication
+            ]),
+            secretOrKey: configService.get('JWT_SECRET'),
+        })
+    }
+
+    async validate({ userId }: tokenPayload) {
+        return await this.userService.getUser({ _id: userId });
+    }
+}
