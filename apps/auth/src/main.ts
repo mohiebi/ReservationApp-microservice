0195746a237f6a5a4727956726d932362b75fcae
Mohi <e.mohamadhosein@gmail.com>
2025-08-13 12:17:15 +0330

    fix some minor issue

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -5,18 +5,17 @@ import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser'
 import { Transport } from '@nestjs/microservices';
-import { options } from 'joi';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   const configService = app.get(ConfigService);
   app.connectMicroservice({
-    Transport: Transport.TCP,
+    transport: Transport.TCP,
     options: {
       host: '0.0.0.0',
-      port: configService.get('TCP_PORT')
-    }
-  })
+      port: configService.get('TCP_PORT'),
+    },
+  });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
