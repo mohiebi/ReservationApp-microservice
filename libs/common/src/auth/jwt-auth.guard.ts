775097c9709c9c570107406d4e0f35f7eccce769
Mohi <e.mohamadhosein@gmail.com>
2025-08-14 19:17:13 +0330

    fixing typo error

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -9,8 +9,7 @@ export class JwtAuthGuard implements CanActivate {
     constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) { }
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const jwt = context.switchToHttp().getRequest().cookie?.Authentication;
-
+        const jwt = context.switchToHttp().getRequest().cookies?.Authentication;
         if (!jwt) {
             return false
         }
