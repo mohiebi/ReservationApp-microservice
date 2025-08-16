3a04b9d6a79fe2bc37b8d1fcc9d9dd86c1566205
Mohi <e.mohamadhosein@gmail.com>
2025-08-16 09:56:35 +0330

    imporing configs

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '@app/common';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        TCP_PORT: Joi.number().required(),
+      })
+    }),
+    LoggerModule
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
