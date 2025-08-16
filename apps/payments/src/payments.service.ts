46987f451af394e6c3755ccbb70d91859ef114a3
Mohi <e.mohamadhosein@gmail.com>
2025-08-16 12:46:40 +0330

    creating careteCharge service

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { number } from 'joi';
+import Stripe from 'stripe';
 
 @Injectable()
 export class PaymentsService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly stripe: Stripe;
+
+  constructor(private readonly configService : ConfigService){
+    this.stripe = new Stripe(
+      this.configService.get<string>('STRIPE_SECRET')!,
+      {
+        apiVersion: '2025-07-30.basil',
+      }
+    );
+  }
+
+  async createCharge(card: Stripe.PaymentMethodCreateParams.Card, amount: number) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    });
+
+    return paymentIntent;
   }
 }
