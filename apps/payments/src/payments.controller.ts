8f61dd9cc5ea252f2541c86429b7ad15422934fd
Mohi <e.mohamadhosein@gmail.com>
2025-08-16 12:54:34 +0330

    add controller

--- apps/payments/src/payments.controller.ts
+++ apps/payments/src/payments.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { createChargeDto } from './dto/create-charge.dto';
 
 @Controller()
 export class PaymentsController {
-  constructor(private readonly paymentsService: PaymentsService) {}
+  constructor(private readonly paymentsService: PaymentsService) { }
 
-  @Get()
-  getHello(): string {
-    return this.paymentsService.getHello();
+  @MessagePattern('create_charge')
+  async createCharge(@Payload() data: createChargeDto) {
+    return this.paymentsService.createCharge(data);
   }
 }
